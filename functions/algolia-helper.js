79328fde9d7244e593e36f8175fbc93a09282221
matt.meagher <matthew.meagher@thestable.com>
2024-06-14 13:10:32 -0500

    feat: test algolia search helper on edgio

---
+++ functions/algolia-helper.js
@@ -0,0 +1,37 @@
+import algoliasearchHelper from 'algoliasearch-helper';
+import algoliasearch from 'algoliasearch';
+
+export async function handleHttpRequest(request, context) {
+  const algoliaParameters = {
+    query: 'seltzer',
+
+    /**
+     * General Config
+     */
+    getRankingInfo: true,
+    analytics: false,
+    enableABTest: false,
+    attributesToRetrieve: "*",
+    attributesToSnippet: "*:20",
+    snippetEllipsisText: "…",
+
+
+    /**
+     * Facet Configuration
+     */
+    maxValuesPerFacet: 100,
+    // facets: [...CONJUNCTIVE_FACETS, ...(facetByIsSearchable ? [IS_SEARCHABLE_FACET] : [])],
+    // disjunctiveFacets: DISJUNCTIVE_FACETS,
+    // disjunctiveFacetsRefinements,
+    // facetsRefinements,
+    distinct: true,
+    facetingAfterDistinct: false, //False so we get all facets for all variants
+    hitsPerPage: 8
+  }
+
+  const client = await algoliasearch(context.environmentVars.ALGOLIA_APP_ID, context.environmentVars.ALGOLIA_ADMIN_KEY);
+  const helper = await algoliasearchHelper(client, 'shopify_products', algoliaParameters);
+  const res = helper.searchOnce();
+  console.log("🚀 ~ handleHttpRequest ~ res:", res)
+  
+}
--- routes.js
+++ routes.js
@@ -28,6 +28,9 @@ export default new Router()
   .match('/example/hello-world-3', {
     edge_function: './functions/edge-function.js',
   })
+  match('/algolia-helper-test', {
+    edge_function: './functions/algolia-helper.js',
+  })
   .match('/example/generate.json', {
     edge_function: './functions/general/generate-json.js',
   })
