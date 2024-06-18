8514861caadd5ae92f2f21edd3a43e1856565214
matt.meagher <matthew.meagher@thestable.com>
2024-06-18 16:36:14 -0500

    Update contentful-algolia.js

--- functions/contentful-algolia.js
+++ functions/contentful-algolia.js
@@ -7,71 +7,71 @@ import resolveResponse from 'contentful-resolve-response';
  * 2. This allows us to "post" entires to an index based on a entry field values. In our case `isSearchable`
  */
 
-const updateIndex = async (request, context, params) => {
-  const searchParams = new URL(request.url).searchParams;
-  const {
-    ALGOLIA_APP_ID: algoliaAppID,
-    ALGOLIA_ADMIN_KEY: algoliaAPIKey,
-    ALGOLIA_INDEX,
-  } = context.environmentVars;
+// const updateIndex = async (request, context, params) => {
+//   const searchParams = new URL(request.url).searchParams;
+//   const {
+//     ALGOLIA_APP_ID: algoliaAppID,
+//     ALGOLIA_ADMIN_KEY: algoliaAPIKey,
+//     ALGOLIA_INDEX,
+//   } = context.environmentVars;
 
-  const indexName = searchParams.get('index_name') || ALGOLIA_INDEX || 'contentful_blogs_pages';
+//   const indexName = searchParams.get('index_name') || ALGOLIA_INDEX || 'contentful_blogs_pages';
 
-  const algoliaQuery = await fetch(
-    `https://${algoliaAppID}-dsn.algolia.net/1/indexes/${indexName}/batch`,
-    {
-      method: 'POST',
-      headers: {
-        'X-Algolia-API-Key': algoliaAPIKey,
-        'X-Algolia-Application-Id': algoliaAppID,
-      },
-      body: JSON.stringify({requests: params}),  
-      edgio: {
-        origin: 'edgio_serverless',
-      }
-    },
-  );
+//   const algoliaQuery = await fetch(
+//     `https://${algoliaAppID}-dsn.algolia.net/1/indexes/${indexName}/batch`,
+//     {
+//       method: 'POST',
+//       headers: {
+//         'X-Algolia-API-Key': algoliaAPIKey,
+//         'X-Algolia-Application-Id': algoliaAppID,
+//       },
+//       body: JSON.stringify({requests: params}),  
+//       edgio: {
+//         origin: 'edgio_serverless',
+//       }
+//     },
+//   );
 
-  const algoliaJSON = await algoliaQuery.json();
-  console.log("🚀 ~ updateIndex ~ algoliaJSON:", algoliaJSON)
+//   const algoliaJSON = await algoliaQuery.json();
+//   console.log("🚀 ~ updateIndex ~ algoliaJSON:", algoliaJSON)
 
-  return algoliaJSON;
-}
+//   return algoliaJSON;
+// }
 
-const fetchContentfulEntries = async (request, context) => {
-  const searchParams = new URL(request.url).searchParams;
-  const {
-    CONTENTFUL_SPACE_ID: spaceID,
-    CONTENTFUL_ACCESS_TOKEN: accessToken,
-    CONTENTFUL_ENVIRONMENT_ID
-  } = context.environmentVars;
+// const fetchContentfulEntries = async (request, context) => {
+//   const searchParams = new URL(request.url).searchParams;
+//   const {
+//     CONTENTFUL_SPACE_ID: spaceID,
+//     CONTENTFUL_ACCESS_TOKEN: accessToken,
+//     CONTENTFUL_ENVIRONMENT_ID
+//   } = context.environmentVars;
 
-  const API_URL = 'https://cdn.contentful.com';
-  const envID = CONTENTFUL_ENVIRONMENT_ID || 'master';
-  const contentType = 'blogPost';
-  const REQUEST_URL = new URL(
-    `${API_URL}/spaces/${spaceID}/environments/${envID}/entries?access_token=${accessToken}&content_type=${contentType}`,
-  );
+//   const API_URL = 'https://cdn.contentful.com';
+//   const envID = CONTENTFUL_ENVIRONMENT_ID || 'master';
+//   const contentType = 'blogPost';
+//   const REQUEST_URL = new URL(
+//     `${API_URL}/spaces/${spaceID}/environments/${envID}/entries?access_token=${accessToken}&content_type=${contentType}`,
+//   );
 
-  const response = await fetch(REQUEST_URL.toString(), {
-    edgio: {
-      origin: 'edgio_serverless',
-    }
-  });
-  const responseJSON = await response.json();
-  const resolved = await resolveResponse(responseJSON);
+//   const response = await fetch(REQUEST_URL.toString(), {
+//     edgio: {
+//       origin: 'edgio_serverless',
+//     }
+//   });
+//   const responseJSON = await response.json();
+//   const resolved = await resolveResponse(responseJSON);
 
-  return resolved;
-}
+//   return resolved;
+// }
 
-const buildAddObjectRequestBody = (entry, objectID) => ({
-  "action": "addObject",
-  "body": {
-    ...entry,
-    // objectID is a Algolia convention. Without this we will add duplicate records
-    "objectID": entry.fields?.slug || entry.fields?.id 
-  },
-})
+// const buildAddObjectRequestBody = (entry, objectID) => ({
+//   "action": "addObject",
+//   "body": {
+//     ...entry,
+//     // objectID is a Algolia convention. Without this we will add duplicate records
+//     "objectID": entry.fields?.slug || entry.fields?.id 
+//   },
+// })
 
 export async function handleHttpRequest(request, context) {
   // const searchParams = new URL(request.url).searchParams;
